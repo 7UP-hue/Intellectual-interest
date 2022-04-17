7823e65df79b80fe1452700f31c12730bd60ffc7
thosewhoyoulove <2406001659@qq.com>
2022-04-17 21:33:41 +0800

    4-17

---
+++ src/pages/admin/classList/addClass.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-dialog v-model="isShow" title="添加班级" width="30%; margin-top: 75px">
+    <el-form>
+      <el-form-item label="班级名">
+        <el-input placeholder="请输入班级名" v-model="name" />
+      </el-form-item>
+      <el-form-item label="管理者">
+        <el-select v-model="value" placeholder="请选择一个管理者">
+    <el-option
+      v-for="item in options"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value"
+    />
+  </el-select>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="isShow = false">取消</el-button>
+        <el-button type="primary" @click="isShow = false">确定</el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+<script lang="ts" setup>
+  import { ref } from 'vue'
+  const isShow = ref(false)
+  const value = ref('')
+  const add = () => {
+    isShow.value = true;
+  }
+  const options = [
+  {
+    value: '张三',
+    label: '张三',
+  },
+  {
+    value: '李四',
+    label: '李四',
+  }
+]
+  defineExpose({add})
+</script>
--- src/pages/admin/classList/index.vue
+++ src/pages/admin/classList/index.vue
@@ -15,47 +15,42 @@
           </el-form>
         </span>
         <span>
-          <el-button :icon="Plus" type="primary" @click="newAdmin">新增</el-button>
+          <el-button :icon="Plus" type="primary" @click="newClass">新增</el-button>
         </span>
       </div>
       <div class="mt-3">
         <el-table :data="adminList">
-          <el-table-column prop="adminName" label="用户名" align="center"></el-table-column>
-          <el-table-column prop="adminPsd" label="密码" align="center"></el-table-column>
+          <el-table-column prop="className" label="班级名" align="center"></el-table-column>
+          <el-table-column prop="classManager" label="管理者" align="center"></el-table-column>
           <el-table-column prop="createTime" label="创建时间" align="center"></el-table-column>
           <el-table-column prop="action" label="操作" align="center">
             <template #default>
-              <el-button type="text" size="small">移除管理员</el-button>
-              <el-button type="text" size="small">删除</el-button>
-              <el-button type="text" size="small" @click="changePsd()">修改密码</el-button>
+              <el-button type="text" size="small">查看班级信息</el-button>
+              <el-button type="text" size="small">解散班级</el-button>
             </template>
           </el-table-column>
         </el-table>
       </div>
     </div>
     <edit-psd ref="editpsd"></edit-psd>
-    <add-admin ref="addadmin"></add-admin>
+    <add-class ref="addclass"></add-class>
   </div>
 </template>
 <script lang="ts" setup>
   import myHeader from '../header.vue'
-  import editPsd from './editPsd.vue'
-  import addAdmin from './addAdmin.vue'
+  import addClass from './addClass.vue'
   import { ref } from 'vue'
   import { Search, Delete, Upload, Plus } from '@element-plus/icons-vue'
   const editpsd = ref(null);
-  const changePsd = () => {
-    (editpsd as any).value.edit();
-  }
-  const addadmin = ref(null)
-  const newAdmin = () => {
-    (addadmin as any).value.add();
+  const addclass = ref(null)
+  const newClass = () => {
+    (addclass as any).value.add();
   }
   import { reactive } from 'vue'
   const adminList = reactive([
-    {adminName: '张三', adminPsd: '123456', createTime: '2021.03.04'},
-    {adminName: '李四', adminPsd: '123456', createTime: '2021.03.04'},
-    {adminName: '王五', adminPsd: '123456', createTime: '2021.03.04'},
-    {adminName: '赵六', adminPsd: '123456', createTime: '2021.03.04'},
+    {className: '一班', classManager: '张三', createTime: '2021.03.04'},
+    {className: '二班', classManager: '李四', createTime: '2021.03.04'},
+    {className: '三班', classManager: '王五', createTime: '2021.03.04'},
+    {className: '四班', classManager: '赵六', createTime: '2021.03.04'}
   ])
 </script>
